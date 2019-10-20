2a05819846e1106965d18079aeebf7627eaa50fc
Vrushali Ghodke <47365588+vrushalighodke@users.noreply.github.com>
2019-10-20 14:31:23 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MoveContols
+    {
+    public partial class Form1 : Form
+        {
+        public Form1()
+            {
+            InitializeComponent();
+            // Move your controls
+            MoveContols.helperClassforControlMover.Init(this.button1);
+			MoveContols.helperClassforControlMover.Init(this.groupBox1);
+            MoveContols.helperClassforControlMover.Init(this.label1);
+            }
+        }
+    }
